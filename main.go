763c6f829edc4d08e04323de0665deaf37626adc
yaqqin <187919872+aanyqn@users.noreply.github.com>
2026-08-16 11:54:14 +0700

    Latihan variabel, struktur data dan pass by reference

--- main.go
+++ main.go
@@ -1,19 +1,81 @@
 package main
 
 import (
-	"log"
+	// "log"
 
-	"github.com/gofiber/fiber/v2"
+	// "github.com/gofiber/fiber/v2"
+
+	"fmt"
 )
 
 func main() {
-	app := fiber.New()
+	// app := fiber.New()
+	// app.Get("/", func(c *fiber.Ctx) error {
+	// 	return c.SendString("Hello World")
+	// })
+	// port := ":3000"
+	// log.Fatal(app.Listen(port))
 
-	var port string
-	app.Get("/", func(c *fiber.Ctx) error {
-		port = "8080"
-		return c.SendString("Server is running")
-	})
+	var string_var string = "Ini String"
+	var int_var int = 12
+	var float_var float64 = 2.34242242
+	var bool_var bool = false
+	var slice_var = []int{1, 2, 3}
 
-	log.Fatal(app.Listen(port))
+	fmt.Println(string_var, int_var, float_var, bool_var, slice_var)
+
+	mahasiswa := make(map[string]int)
+
+	//Menambah ke map
+	mahasiswa["Joko"] = 89
+	mahasiswa["Wawan"] = 90
+	mahasiswa["Andi"] = 88
+
+	//Membaca dan cek keberadaan
+	bacaNilai := mahasiswa["Joko"]
+	fmt.Println(bacaNilai)
+	if nilaiJoko, exist := mahasiswa["Joko"]; exist {
+		fmt.Println("Nilai Joko adalah:", nilaiJoko)
+	} else {
+		fmt.Println("Joko belum memiliki nilai")
+	}
+
+	//Menghaus isi map
+	delete(mahasiswa, "Joko")
+
+	//Menelusuri isi map
+	for nama, nilai := range mahasiswa {
+		fmt.Printf("Nama: %s || Nilai: %d \n", nama, nilai)
+	}
+
+	a, b := 100, 150
+	fmt.Printf("1. Nilai awal \n Nilai A: %d || Nilai B: %d \n", a, b)
+	swapByValue(a, b)
+	fmt.Printf("a. swap dengan pass by value \n Nilai A: %d || Nilai B: %d \n", a, b)
+	swap(&a, &b)
+	fmt.Printf("b. swap dengan pass by pointer \n Nilai A: %d || Nilai B: %d \n", a, b)
+
+	makanan := make([]string, 0, 10)
+	makanan = append(makanan, "Rendang", "Nasi_Goreng")
+	fmt.Println("2. slice awal", makanan)
+	updateSliceByValue(makanan, "Bakso")
+	fmt.Println("a. update slice dengan pass by value \n", makanan)
+	updateSlice(&makanan, "Bakso")
+	fmt.Println("b. update slice dengan pass by pointer \n", makanan)
+}
+
+func swap(a, b *int) {
+	*a, *b = *b, *a
+}
+
+func swapByValue(a, b int) {
+	a, b = b, a
+}
+
+func updateSlice(s *[]string, newItem string) {
+	*s = append(*s, newItem)
+}
+
+func updateSliceByValue(s []string, newItem string) {
+	s = append(s, newItem)
 }
